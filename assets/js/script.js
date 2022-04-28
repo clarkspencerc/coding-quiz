a1dd43ea75c64ab841d62a37600cad7695d5e269
clarkspencerc <clarkspencerc@gmail.com>
2022-04-28 15:39:16 -0600

    display first question and options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,60 @@
+var welcomeEl = document.querySelector("#welcome"); 
 var quizEl = document.querySelector("#quiz"); 
 var timerEl = document.querySelector("#timer"); 
 var questions = []
+
+// question bank 
+
+questions = [
+    {
+        q: "'box-sizing' is a property that changes the calculation of which values of the element?",
+        ans: {
+            a:"width",
+            b:"height",
+            c:"margin",
+            d:"Both width and height"
+        },
+        correctAnswer:'d'
+    },
+    {
+        q: "If you need to position a child element in relation to its parent element, which value shoulud you set the position property to?",
+        ans: {
+            a:"display",
+            b:"relative",
+            c:"absolute",
+            d:"static"
+        },
+        correctAnswer:'b'
+    },
+    {
+        q: "What is an example of a pseudo-element",
+        ans: {
+            a:"::before",
+            b:"::after",
+            c:"::first-letter",
+            d:"All of the above"
+        },
+        correctAnswer:'d'
+    },
+]
+
 var timeLeft = questions.length * 10 + 10 ; 
 
 
 
 // start game 
-//might need to pass in an event and event.preventDefault(): 
 var startGame = function(){
 var h1El = document.createElement("h1"); 
 h1El.textContent = "Welcome to The Coding Quiz";
-quizEl.appendChild(h1El); 
+welcomeEl.appendChild(h1El); 
 
 var buttonEl = document.createElement("button"); 
 buttonEl.textContent = "Start Quiz"; 
 buttonEl.setAttribute("id", "start-quiz");
-quizEl.appendChild(buttonEl); 
-hide('start-quiz','quiz');
-
-}
+welcomeEl.appendChild(buttonEl); 
+hide('start-quiz','welcome');
+displayQuestion(); 
+};
 
 
 // hide function 
@@ -33,18 +69,34 @@ var hide = function(buttonId, divId){
 var startTimer = function(){
 var setStartEl = setInterval(function(){
     if (timeLeft <0){
-        clearInterval(setStart); 
+        clearInterval(setStartEl); 
     }
     else{
         document.getElementById("timer").innerHTML = "Seconds Remaining: " + timeLeft;
     }
     timeLeft -= 1; 
 }, 1000);
-timerEl.appendChild(setStartEl); 
 };
 
+//display question 
 
-// question bank 
+var displayQuestion = function(){
+   var question = document.createElement("h2"); 
+   question.textContent = questions[1].q; 
+   quizEl.appendChild(question); 
+  
+
+   for(letter in questions[1].ans){
+    var answer = document.createElement("button");
+    answer.textContent= questions[1].ans[letter];
+    // need to set a class that way I can tell which one is correct 
+    //answer.setAttribute("class", ans[letter]); 
+    console.log(questions[1].ans[letter]); 
+    console.log()
+    quizEl.appendChild(answer);}
+
+
+};
 
 // ask question function loops through question bank until it's displayed all of them 
 
@@ -61,3 +113,4 @@ timerEl.appendChild(setStartEl);
 // retrive score from local data 
 
 startGame(); 
+
