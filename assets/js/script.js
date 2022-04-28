2138f4e5753a67d4ce8629a609cb05325cd13d3b
clarkspencerc <clarkspencerc@gmail.com>
2022-04-28 13:10:15 -0600

    added start button, hide function and timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-var pageContentEl = document.querySelector("#page-content"); 
+var quizEl = document.querySelector("#quiz"); 
+var timerEl = document.querySelector("#timer"); 
+var questions = []
+var timeLeft = questions.length * 10 + 10 ; 
+
 
 
 // start game 
@@ -6,15 +10,38 @@ var pageContentEl = document.querySelector("#page-content");
 var startGame = function(){
 var h1El = document.createElement("h1"); 
 h1El.textContent = "Welcome to The Coding Quiz";
-pageContentEl.appendChild(h1El); 
+quizEl.appendChild(h1El); 
 
 var buttonEl = document.createElement("button"); 
 buttonEl.textContent = "Start Quiz"; 
-buttonEl.setAttribute("id", "submit");
-pageContentEl.appendChild(buttonEl); 
+buttonEl.setAttribute("id", "start-quiz");
+quizEl.appendChild(buttonEl); 
+hide('start-quiz','quiz');
+
 }
 
+
+// hide function 
+var hide = function(buttonId, divId){
+    document.getElementById(buttonId).onclick = function(){
+        document.getElementById(divId).hidden = true
+    }
+};
+
 // start timer function 
+
+var startTimer = function(){
+var setStartEl = setInterval(function(){
+    if (timeLeft <0){
+        clearInterval(setStart); 
+    }
+    else{
+        document.getElementById("timer").innerHTML = "Seconds Remaining: " + timeLeft;
+    }
+    timeLeft -= 1; 
+}, 1000);
+timerEl.appendChild(setStartEl); 
+};
 
 
 // question bank 
